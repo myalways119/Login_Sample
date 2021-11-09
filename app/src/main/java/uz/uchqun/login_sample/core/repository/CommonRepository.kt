98f906b317145ac94cc76d61d779a6c20ac14e3c
Ho <myalways119>
2021-11-09 22:54:52 +0900

    1

--- app/src/main/java/uz/uchqun/login_sample/core/repository/CommonRepository.kt
+++ app/src/main/java/uz/uchqun/login_sample/core/repository/CommonRepository.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import uz.uchqun.login_sample.core.network.CommonApi
 import uz.uchqun.login_sample.core.network.RetrofitClient
 
-class CommonRepository(context: Context) : BaseRepository()
+class CommonRepository() : BaseRepository()
 {
-
     val api: CommonApi = RetrofitClient.client!!.create(CommonApi::class.java)
 
+    suspend fun SearchUserInfo(phoneNo:String) = safeApiCall {
+        api.SelectUserInfo("SELECT_USER", phoneNo)
+    }
 }
